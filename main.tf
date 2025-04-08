7cece4a7077eee058d4e3c6f7cf3ac86a83a202d
leogarcia2024 <leogarcia3245626@gmail.com>
2025-04-08 00:02:46 -0500

    actualizar puerto 4010

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "docker_container" "container_server_web" {
   name  = "server_web"
   image = "nginx:latest"
   ports {
-    external = 4000
+    external = 4010
     internal = 80
 
   }
